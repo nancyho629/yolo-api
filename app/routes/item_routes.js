4d7929098c3077dec3608ab0df7ffc9242f9d165
nancyho629 <nancyho629@gmail.com>
2019-09-30 13:00:46 -0400

    Prevent user from changing resource owner
    Added line to delete item.user upon patch to prevent user from
    changing their own resource to have another owner

--- app/routes/item_routes.js
+++ app/routes/item_routes.js
@@ -52,7 +52,7 @@ router.post('/items', requireToken, (req, res, next) => {
 router.patch('/items/:id', requireToken, removeBlanks, (req, res, next) => {
   // if the client attempts to change the `owner` property by including a new
   // owner, prevent that by deleting that key/value pair
-  // delete req.body.example.owner
+  delete req.body.item.owner
 
   Item.findById(req.params.id)
     .then(handle404)
